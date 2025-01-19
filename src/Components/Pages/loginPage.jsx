892df30c25055c1b62de5a998059d4ef3ebdabad
ulgmaduwantha <u.l.g.maduwantha@gmail.com>
2025-01-19 22:12:22 +0530

    Successfully installed tailwind CSS 😎

--- src/Components/Pages/loginPage.jsx
+++ src/Components/Pages/loginPage.jsx
@@ -6,7 +6,7 @@ export default function(){
         <div>
             <h1>Login Page</h1>
             <input type="text" placeholder="Enter your Username" />
-            <input type = "password" placeholder="Enter your password"></input>
+            <input type = "password"  placeholder="Enter your password"></input>
             <button>Login</button>
             <Link to="/">back to home Page</Link>
         </div>
