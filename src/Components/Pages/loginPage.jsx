1814fbe6d6198f22d7a5b0347ce35f68fde0999d
ulgmaduwantha <u.l.g.maduwantha@gmail.com>
2025-01-30 12:07:08 +0530

    Created simple login page 😎

--- src/Components/Pages/loginPage.jsx
+++ src/Components/Pages/loginPage.jsx
@@ -1,42 +1,52 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function LoginPage() {
+
+    const [email,setEmail] = useState("Your Email");
+    const [password,setPassword] = useState("Your Password");
+
+    function login(){
+        axios.post("http://localhost:3000/api/users/login",{
+            email:email,
+            password:password
+        }).then(
+            (res)=>{
+                console.log(res)
+                if(res.data.user == null){
+                    alert(res.data.messege)
+                    return
+                }
+                localStorage.setItem("token",res.data.token)
+                if(res.data.user.type == "admin"){
+                    window.location.href = "/admin"
+                }else{
+                    window.location.href = "/"
+                }
+            }
+        )
+    }
+
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="w-96 p-6 bg-white rounded-lg shadow-md border border-gray-200">
-                <h1 className="text-2xl font-bold text-center mb-6 text-gray-700">Login Page</h1>
-
-                <div className="mb-4">
-                    <input
-                        type="text"
-                        placeholder="Enter your Username"
-                        className="w-full px-4 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
-                </div>
-
-                <div className="mb-6">
-                    <input
-                        type="password"
-                        placeholder="Enter your password"
-                        className="w-full px-4 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
-                </div>
-
-                <button
-                    className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                >
-                    Login
-                </button>
-
-                <div className="mt-4 text-center">
-                    <Link
-                        to="/"
-                        className="text-blue-500 hover:underline"
-                    >
-                        Back to Home Page
-                    </Link>
-                </div>
+        <div className="flex justify-center items-center w-full h-screen bg-red-900">
+            <div className="flex flex-col justify-center items-center w-[450px] h-[350px] bg-blue-600">
+                <img src="/Logo.jpg" className="rounded-full h-[100px] w-[100px]" />
+                <span>Email</span>
+                <input defaultValue={email} onChange={
+                    (e) =>{
+                        setEmail(e.target.value)
+                    }
+                }/>
+                <span>Password</span>
+                <input defaultValue={password} type="password" onChange={
+                    (e) =>{
+                        setPassword(e.target.value)
+                    }
+                }/>
+                <button onClick={login} className="bg-green-500 hover:bg-green-700 text-white font-bold py-">Login</button>
             </div>
+
         </div>
     );
 }
